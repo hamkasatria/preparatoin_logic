fec5bf139a87811f22e1639744df16637b876d27
hamkasatria <muhhamkasatrianto@gmail.com>
2022-06-08 23:06:14 +0700

    add other function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # preparatoin_logic
 preparation logic inteview
+
+## Link Case
+`https://refactory.sev-2.com/lobby/stickit/439/`
--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 	"unicode"
 )
 
 func main() {
 	// fmt.Println("Hello world")
-	// alt case
+	//BigNumber
+	GetBiggestNUmber(592)
 
+	//MapData
+	MapArrayData([]int{15, 1, 3})
+
+	// alt case
 	AltCase("Hello World")
 
 	//mutiplecase
@@ -20,19 +26,45 @@ func main() {
 	NeFibbon([]int32{15, 1, 3})
 
 	//rev
+	Rev("Hallo World")
+
+	//midalf
+	MidAlf("Q", "Z")
 }
 
-// func GetBiggestNUmber(data int32) {
+func GetBiggestNUmber(data int) {
+	num, _ := strconv.Atoi(*helperBigNUmber(data))
+	fmt.Println(num)
+}
 
-// 	num := helperBigNUmber(data)
-// 	fmt.Println(num)
-// }
+func helperBigNUmber(data int) *string {
+	stt := strconv.Itoa(data)
+	arr := []rune(stt)
+	var arrInt []int
+	var arrStr []string
+	if len(arr) < 3 {
+		return nil
+	}
 
-// func helperBigNUmber(data int32) int32 {
-// 	string := strconv.Itoa(data)
+	for _, v := range arr {
+		num, _ := strconv.Atoi(string(v))
+		if num <= 0 {
+			return nil
+		}
+		arrInt = append(arrInt, num)
+	}
 
-// 	return nil(string)
-// }
+	sort.Slice(arrInt, func(i, j int) bool {
+		return int(arrInt[j]) < int(arrInt[i])
+	})
+
+	for _, v := range arrInt {
+		arrStr = append(arrStr, strconv.Itoa(v))
+	}
+
+	all := strings.Join(arrStr, "")
+	return &all
+}
 
 func MapArrayData(data []int) {
 	sort.Slice(data, func(i, j int) bool {
@@ -87,15 +119,43 @@ func Fib(sum int32, n1 int32, n2 int32) int32 {
 	return Fib(sum, n2, count)
 }
 
-// func Rev(data string) string {
-// 	arr := []rune(data)
-// 	var newArr []string
-// 	for _, v := range arr {
-// 		if unicode.IsUpper(v) {
-// 			newArr = append(newArr, strings.ToLower(string(v)))
-// 		} else {
-// 			newArr = append(newArr, strings.ToUpper(string(v)))
-// 		}
-// 	}
-// 	fmt.Println(strings.Join(newArr, ""))
-// }
+func Rev(data string) {
+	var arrStr []string
+	arrSt := strings.Fields(data)
+	for _, v := range arrSt {
+		var tempSt []string
+		arr := []rune(v)
+		for i, w := range arr {
+			if unicode.IsUpper(w) {
+				tempSt = append(tempSt, strings.ToUpper(string(arr[len(arr)-i-1])))
+			} else {
+				tempSt = append(tempSt, strings.ToLower(string(arr[len(arr)-i-1])))
+			}
+		}
+		arrStr = append(arrStr, strings.Join(tempSt, ""))
+	}
+	fmt.Println(strings.Join(arrStr, " "))
+}
+
+func MidAlf(fist string, last string) {
+	const Alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	arrAlf := []rune(Alf)
+	var firstQue, lastQue int
+	for i, v := range arrAlf {
+		if string(v) == fist {
+			firstQue = i
+		}
+		if string(v) == last {
+			lastQue = i
+		}
+	}
+	sumQue := firstQue + lastQue
+	avgQue := sumQue / 2
+
+	if sumQue%2 != 0 {
+		fmt.Print(string(arrAlf[avgQue]), string(arrAlf[avgQue+1]))
+	} else {
+		fmt.Print(string(arrAlf[avgQue]))
+	}
+
+}
